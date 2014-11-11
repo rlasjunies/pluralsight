fe17592f731a277aba6961db5cb11af063153840
Richard Lasjunies <rlasjunies@gmail.com>
2014-11-11 17:42:34 +0100

    init

---
+++ firefoxos/ProteinTrackerHosted/ProteinTrackerHosted/Controllers/StaticContentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProteinTrackerHosted.Controllers
+{
+    public class StaticContentController : Controller
+    {
+        // GET: StaticContent
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ firefoxos/ProteinTrackerHosted/ProteinTrackerHosted/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProteinTrackerHosted.MvcApplication" Language="C#" %>
