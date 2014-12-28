199995f0beb9a91f2c3ad2251344fc741d9e42b1
Richard Lasjunies <rlasjunies@gmail.com>
2014-12-28 21:47:36 +0100

    with the config files

---
+++ jwt/jwt-server/services/config.d.ts
@@ -0,0 +1,9 @@
+declare var constants: {
+    FACEBOOK_SECRET: string;
+    GOOGLE_SECRET: string;
+    JWT_SECRET: string;
+    EMAIL_SECRET: string;
+    SMTP_PASS: string;
+    APP_URL: string;
+};
+export = constants;
